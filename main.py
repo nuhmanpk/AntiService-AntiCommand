9ddd0feeffbcf3712f1e61b5d3f9891382fdbc40
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-08-07 19:20:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import pyrogram
 from pyrogram import Client, filters
-from pyrogram.types import User, Message
-
-    
+from pyrogram.types import User, Message   
 bughunter0 = Client(
     "Sticker-Bot",
     bot_token = os.environ["BOT_TOKEN"],
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"]
 )
-
 @bughunter0.on_message(filters.command(["start"]))
 async def start(bot, update):
  await message.reply_text("Ready to Work")
